36b8ae1164b35e6396486e0917ac7f6e17d852da
Danny F <59851122+0dannyboi@users.noreply.github.com>
2021-01-08 18:34:21 -0600

    Update elliptic integral vs number of points.py
    Updated to now calculate the force between concentric rings.

--- Python/Monte Carlo/Charge Distribution/elliptic integral vs number of points.py
+++ Python/Monte Carlo/Charge Distribution/elliptic integral vs number of points.py
@@ -4,8 +4,8 @@ import scipy.special as sp
 import time
 
 
-rp = 10
-rq = 5
+rp = 5
+rq = 10
 q_p = 1
 q_q = 3
 pi = np.pi
@@ -13,16 +13,18 @@ pi = np.pi
 
 def point_forces(n):
     dq = q_q / n
+    dqp = q_p / n
     d_theta = 2 * pi / n
+    f_t = 0
+    p = d_theta / 2
     p_r = [rp, 0]
     f = 0
-    for a in range(0, n):
+    for a in range(1, n):
         q_r = [rq * round(np.cos(a * d_theta),6), rq * round(np.sin(a * d_theta), 6)]
-        #print(q_r)
-        v = list(np.subtract(p_r, q_r))
-        norm = np.linalg.norm(v)
-        f = f + dq * q_p * v[0] / (norm ** 3)
-    #print(f)
+        rad = list(np.subtract(p_r, q_r))
+        norm = np.linalg.norm(rad)
+        f = f + dq * dqp * rad[0] / (norm ** 3)
+    f = f * n
     return f
 
 
@@ -36,18 +38,18 @@ def ring_force():
 
 
 def main():
-    x = list(np.arange(4, 40, 1))
+    x = list(np.arange(50, 500, 1))
     point_n = []
-    t1_point = time.time()
+    #t1_point = time.time()
     for a in (x):
         point_n.append(point_forces(a))
-    t2_point = time.time()
+    #t2_point = time.time()
     rf = ring_force()
-    t2_ring = time.time()
-    point_t = t2_point - t1_point
-    ring_t = t2_ring - t2_point
-    print("point " + str(point_t))
-    print("ring " + str(ring_t))
+    #t2_ring = time.time()
+    #point_t = t2_point - t1_point
+    #ring_t = t2_ring - t2_point
+    #print("point " + str(point_t))
+    #print("ring " + str(ring_t))
     y2 = list(np.add(rf, np.zeros(len(x))))
     plt.plot(x, point_n)
     plt.plot(x, y2)
